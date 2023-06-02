ea57206d62a88c4aca3569fc5547f18bfd9fd7c6
Stew Eucen <stew.eucen@gmail.com>
2023-06-02 15:36:45 +0900

    Add setupAfterEnv.js to setupFilesAfterEnv of jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   setupFilesAfterEnv: [
     '@openreachtech/renchan-test-tools/lib/environment/setupAfterEnv.js',
+    './lib/environment/setupAfterEnv.js',
   ],
   testPathIgnorePatterns: [
     '/node_modules/'
