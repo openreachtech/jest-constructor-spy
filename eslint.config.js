3439f463e4827487c7a31c9dc173c0980696962d
Stew Eucen <stew.eucen@gmail.com>
2024-06-19 00:31:05 +0900

    Add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const configurations = require('@openreachtech/eslint-config')
+
+module.exports = [
+  /*
+   * If ignores is used without any other keys in the configuration object, then the patterns act as global ignores. Here’s an example:
+   *
+   * https://eslint.org/docs/latest/use/configure/configuration-files#globally-ignoring-files-with-ignores
+   */
+  {
+    ignores: [
+      'index.mjs',
+    ],
+  },
+
+  ...configurations,
+]
